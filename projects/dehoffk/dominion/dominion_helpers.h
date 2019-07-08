9c2d573e82a2701377594c2cf78d2957f3dbfe11
Keith Dehoff <dehoffk@oregonstate.edu>
2019-07-07 22:16:18 -0700

    refactored dominion.c and dominion_helpers.h, and added bugs to dominion.c

--- projects/dehoffk/dominion/dominion_helpers.h
+++ projects/dehoffk/dominion/dominion_helpers.h
@@ -11,5 +11,10 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
+int baronCard(int choice1, struct gameState *state, int currentPlayer);
+int minionCard(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int ambassadorCard(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
+int tributeCard(struct gameState *state, int currentPlayer);
+int mineCard(int choice1, int choice2, struct gameState *state, int handPos, int currentPlayer);
 
 #endif
